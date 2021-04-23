4de808c6c23b8de3032e4451964d63ec72954871
Alvaro Muhlethaler <alvaro.muhlethaler@gmail.com>
2021-04-23 15:23:33 +0100

    Adds lua config for nvim

---
+++ vim/nvim-lua/lua/lsp/kotlin-ls.lua
@@ -0,0 +1,8 @@
+require'lspconfig'.kotlin_language_server.setup{}
+
+  Commands:
+
+  Default Values:
+    cmd = { "kotlin-language-server" }
+    filetypes = { "kotlin" }
+    root_dir = root_pattern("settings.gradle")
